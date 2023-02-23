2c7aed1dcb368f586b363df52cd001e50f1f3075
MatLeg <mat.legierskii@gmail.com>
2023-02-23 22:33:20 +0100

    typesense version beta5 implemented, UI updated, refactor

--- app/src/main/java/com/example/androidtypesense/MainActivity.kt
+++ app/src/main/java/com/example/androidtypesense/MainActivity.kt
@@ -1,37 +1,113 @@
 package com.example.androidtypesense
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import com.example.androidtypesense.ui.main.MainFragment
-import kotlinx.coroutines.*
+import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var typesenseClient: TypesenseInitializer
+    private val COLLECTION_NAME = "intermariumCountries"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+
+        //Init Typesense client
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO) {
+                try {
+                    typesenseClient = TypesenseInitializer()
+                    val connectionState = typesenseClient.connectionState
+                    val connectionStateMsg = "Connection state = $connectionState"
+                    runOnUiThread {
+                        //display connection state, if success sets the UI functions
+                        Toast.makeText(this@MainActivity, connectionStateMsg, Toast.LENGTH_SHORT).show()
+                        if (connectionState) setUpUi()
+                    }
+                } catch (e: Exception) {
+                    e.message?.let { Log.e("ts", it) }
+                    e.printStackTrace()
+                }
+            }
         }
 
-        GlobalScope.launch {
+    }
 
+    private fun setUpUi() {
+        //set action when click search icon
+        findViewById<ImageView>(R.id.ic_search)?.setOnClickListener { onSearchIconClick() }
+        //set action when typing
+        findViewById<EditText>(R.id.et_search)?.doOnTextChanged { text, _, _, _ -> search(text.toString()) }
+    }
+
+    private fun search(text: String, showDetails: Boolean = false) {
+        Log.i("ts", "Searching text: $text")
+        lifecycleScope.launch {
+
+            var resultString = ""
+            var details = ""
+
+            //do search and map response to resultString
             withContext(Dispatchers.IO) {
-                //Init Typesense
-                val client = TypesenseInitializer()
+                typesenseClient.search(COLLECTION_NAME, text)?.let { result ->
+                    result.hits.forEachIndexed { index, searchResultHit ->
+                        resultString += "${index + 1}. ${searchResultHit.document["countryName"]}, ${searchResultHit.document["capital"]} \n"
+                    }
+                    details = "Found: ${result.found} in ${result.searchTimeMs}ms"
+                }
+            }
 
-                delay(2000) //wait till client will be ready - temporary solution :)
-
-                Log.w("elox","Test = $client")
-
-                //client.createDoc()
-
-                client.retrieve()
+            //update UI
+            runOnUiThread {
+                if (showDetails) Toast.makeText(this@MainActivity, details, Toast.LENGTH_SHORT).show()
+                updateResults(resultString)
             }
 
         }
     }
+
+    private fun onSearchIconClick() {
+        val text = findViewById<EditText>(R.id.et_search).text.toString()
+        search(text, true)
+        hideKeyboard()
+    }
+
+    private fun updateResults(results: String) {
+        findViewById<TextView>(R.id.tv_results).text = results
+    }
+
+    private fun hideKeyboard() {
+        val view = findViewById<EditText>(R.id.et_search)
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/androidtypesense/TypesenseInitializer.java
+++ app/src/main/java/com/example/androidtypesense/TypesenseInitializer.java
@@ -1,12 +1,9 @@
 package com.example.androidtypesense;
 
-import android.util.Log;
-
 import org.typesense.api.*;
 import org.typesense.resources.*;
 import org.typesense.model.*;
 
-import java.lang.Override;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,67 +12,36 @@ public class TypesenseInitializer {
 
     private Client client;
 
-    private String XXX = null;//TODO
-    private String API_KEY = null;//TODO
+    private String XXX = null; //TODO
+    private String API_KEY = null; //TODO
 
 
-    public TypesenseInitializer() {
-        Thread thread = new Thread(new Runnable() {
+    public TypesenseInitializer() throws Exception {
+        List<Node> nodes = new ArrayList<>();
+        nodes.add(
+                new Node(
+                        "https",       // For Typesense Cloud use https
+                        XXX,  // For Typesense Cloud use xxx.a1.typesense.net
+                        "443"        // For Typesense Cloud use 443
+                )
+        );
 
-            @Override
-            public void run() {
-                try {
-                    List<Node> nodes = new ArrayList<>();
-                    nodes.add(
-                            new Node(
-                                    "https",       // For Typesense Cloud use https
-                                    XXX,  // For Typesense Cloud use xxx.a1.typesense.net
-                                    "443"        // For Typesense Cloud use 443
-                            )
-                    );
-
-                    Configuration configuration = new Configuration(nodes, Duration.ofSeconds(2), API_KEY);
-                    client = new Client(configuration);
-                    System.out.println(client.health.retrieve().get("ok"));
-                    Log.w("ts", client.health.retrieve().get("ok").toString());
-                } catch (Exception e) {
-                    Log.e("ts", e.getMessage());
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        thread.start();
+        Configuration configuration = new Configuration(nodes, Duration.ofSeconds(2), API_KEY);
+        client = new Client(configuration);
+        System.out.println(client.health.retrieve().get("ok"));
     }
 
-    public void createDoc() throws Exception {
-        List<Field> fields = new ArrayList<>();
-        fields.add(new Field().name("countryName").type(FieldTypes.STRING));
-        fields.add(new Field().name("capital").type(FieldTypes.STRING));
-        fields.add(new Field().name("gdp").type(FieldTypes.INT32).facet(true).sort(true));
-
-        CollectionSchema collectionSchema = new CollectionSchema();
-        collectionSchema.name("countries").fields(fields).defaultSortingField("gdp");
-
-        client.collections().create(collectionSchema);
+    public Boolean getConnectionState() throws Exception {
+        return (Boolean) client.health.retrieve().get("ok");
     }
 
-    public void retrieve() throws Exception {
-        CollectionResponse countries = client.collections("countries").retrieve();
-        CollectionResponse[] retrieve = client.collections().retrieve();
-        if(countries != null) {
-            Log.w("ts",countries.getName());
-            Log.w("ts",countries.getNumDocuments().toString());
-            Log.w("ts",countries.getCreatedAt().toString());
-        } else {
-            Log.w("ts"," countries = NULL");
-        }
-
-        if(retrieve != null) {
-            Log.w("ts", String.valueOf(retrieve.length));
-        } else {
-            Log.w("ts"," retrieve = NULL");
-        }
+    public SearchResult search(String collectionName, String queryText) throws Exception {
+        SearchParameters searchParameters = new SearchParameters()
+                .q(queryText)
+                .queryBy("countryName,capital")
+                .prefix("true,true")
+                .perPage(20);
+        return client.collections(collectionName).documents().search(searchParameters);
     }
 
 
